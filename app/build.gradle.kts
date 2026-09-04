bb56f56c9d8b101455c91c7a2f287e3c1ff2f8bb
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-04 10:19:13 -0500

    feat: add internet permissions and Coil dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,8 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+
+    // Coil
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
 }
