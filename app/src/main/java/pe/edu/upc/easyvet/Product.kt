539120b787c9077a34f1a14aa778a79fcc75570e
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-04 10:01:16 -0500

    feat: add Product data class

---
+++ app/src/main/java/pe/edu/upc/easyvet/Product.kt
@@ -0,0 +1,10 @@
+package pe.edu.upc.easyvet
+
+data class Product(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val price: Double,
+    val rating: Double,
+    val imageUrl: String
+)
