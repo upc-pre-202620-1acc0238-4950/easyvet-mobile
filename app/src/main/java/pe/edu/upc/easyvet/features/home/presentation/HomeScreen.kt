bd6df9c744d6caa5dd21cd9193fb838d30d28234
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-08 21:59:57 -0500

    chore: remove unused color definitions and streamline color schemes

--- app/src/main/java/pe/edu/upc/easyvet/features/home/presentation/HomeScreen.kt
+++ app/src/main/java/pe/edu/upc/easyvet/features/home/presentation/HomeScreen.kt
@@ -3,22 +3,16 @@ package pe.edu.upc.easyvet.features.home.presentation
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
-import coil3.compose.AsyncImage
 import pe.edu.upc.easyvet.core.theme.EasyVetTheme
 
 @Composable
