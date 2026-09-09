1b353cec593a621a8a17f8d338d6a690e23fd01f
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-08 21:57:18 -0500

    feat(Home): refactor HomeScreen to use ProductCard component and improve loading state handling

--- app/src/main/java/pe/edu/upc/easyvet/features/home/presentation/HomeScreen.kt
+++ app/src/main/java/pe/edu/upc/easyvet/features/home/presentation/HomeScreen.kt
@@ -1,5 +1,6 @@
 package pe.edu.upc.easyvet.features.home.presentation
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -7,8 +8,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,35 +27,30 @@ fun HomeScreen(modifier: Modifier = Modifier, viewModel: HomeViewModel = viewMod
     val uiState = viewModel.uiState.collectAsStateWithLifecycle().value
 
 
-    LazyColumn(modifier = modifier.fillMaxSize()) {
-        item {
-            Text(text = "Home Screen")
-        }
-
-        items(10) { index ->
-            Text(text = "Item $index")
-        }
-
-        items(uiState.products) { product ->
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth(0.45f)
-                    .padding(8.dp)
-            ) {
-                Column(modifier = Modifier.padding(8.dp)) {
-                    AsyncImage(
-                        model = product.imageUrl,
-                        contentDescription = product.name,
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                    Text(text = product.name, fontWeight = FontWeight.Bold, maxLines = 1)
-                    Text(text = "${product.rating}")
-                    Text(text = "$ ${product.price}", fontWeight = FontWeight.Bold)
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        when {
+            uiState.products.isNotEmpty() -> {
+                LazyColumn(modifier = modifier.fillMaxSize()) {
+                    items(uiState.products) { product ->
+                        ProductCard(product = product)
+                    }
                 }
+            }
 
+            uiState.isLoading -> {
+                CircularProgressIndicator()
+            }
+
+            else -> {
+                Text(text = uiState.errorMessage ?: "No products available")
             }
         }
     }
+
 }
 
 @Preview(showBackground = true)
---
+++ app/src/main/java/pe/edu/upc/easyvet/features/home/presentation/ProductCard.kt
@@ -0,0 +1,41 @@
+package pe.edu.upc.easyvet.features.home.presentation
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import pe.edu.upc.easyvet.features.home.domain.Product
+
+@Composable
+fun ProductCard(modifier: Modifier = Modifier, product: Product) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Column(modifier = Modifier.padding(8.dp)) {
+            AsyncImage(
+                model = product.imageUrl,
+                contentDescription = product.name,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+            )
+            Text(
+                text = product.name,
+                fontWeight = FontWeight.Bold,
+                maxLines = 1
+            )
+            Text(text = "${product.rating}")
+            Text(text = "$ ${product.price}", fontWeight = FontWeight.Bold)
+        }
+
+    }
+}
