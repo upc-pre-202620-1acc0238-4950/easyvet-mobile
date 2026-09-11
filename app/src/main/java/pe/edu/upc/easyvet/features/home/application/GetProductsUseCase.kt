880711a03faa80959cc224e38f80ad232281ec49
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-11 10:24:21 -0500

    chore: remove unused InMemoryRepository import from GetProductsUseCase

--- app/src/main/java/pe/edu/upc/easyvet/features/home/application/GetProductsUseCase.kt
+++ app/src/main/java/pe/edu/upc/easyvet/features/home/application/GetProductsUseCase.kt
@@ -1,7 +1,6 @@
 package pe.edu.upc.easyvet.features.home.application
 
 import pe.edu.upc.easyvet.features.home.domain.ProductRepository
-import pe.edu.upc.easyvet.features.home.infrastructure.repositories.InMemoryRepository
 import pe.edu.upc.easyvet.features.home.infrastructure.repositories.ProductRepositoryImpl
 
 class GetProductsUseCase(private val repository: ProductRepository = ProductRepositoryImpl()) {
