cf0df57f05c5e056286c2ed3c2052134d0d90d93
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-11 09:46:25 -0500

    chore: add Retrofit and Gson converter dependencies to build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,9 @@ dependencies {
     // ViewModel Compose
     implementation(libs.androidx.lifecycle.viewmodel.compose)
 
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+
 }
