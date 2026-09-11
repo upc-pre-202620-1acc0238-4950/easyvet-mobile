f4591e1ad31aaa09ae8f8c2ca1b55d78aba74685
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-11 10:41:20 -0500

    chore: add Hilt dependencies and plugin to build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -65,6 +66,10 @@ dependencies {
     // Retrofit
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
 
 
 }
