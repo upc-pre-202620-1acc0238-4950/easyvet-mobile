47c1902128cbeaa8b8f8f5bb978938886a2b58b9
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-25 10:23:47 -0500

    feat: add Room dependencies for database support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,4 +73,7 @@ dependencies {
     ksp(libs.hilt.android.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
 
+    // Room
+    implementation(libs.room.runtime)
+    ksp(libs.room.compiler)
 }
